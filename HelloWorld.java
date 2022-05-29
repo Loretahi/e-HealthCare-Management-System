47169128d61d37084a9713a5c34a7304cec4ebb6
Loretahi <loreta_hi@abv.bg>
2022-05-29 18:45:27 +0300

    test2

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Talk to me Java!");
 
-        int age = 25;
+        int age = 252333;
     }
 }
